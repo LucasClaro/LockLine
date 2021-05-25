d9b94d6d6e08c10c2417745cc783c0f1dd91d121
Sayuri Hioki <sayuri.hioki12@gmail.com>
2021-05-25 16:19:25 -0300

    fim da base

--- LockLine/Fases/Esteira.swift
+++ LockLine/Fases/Esteira.swift
@@ -12,6 +12,54 @@ extension GameScene{
     
     //MARK: Cubo aproximado
     func DrawEsteiraAberto() {
+        
+        //PLACAS
+        if navegação.Esteira.placasCores[0] == "azul" {
+            let placaEsquerda = SKSpriteNode(imageNamed: "azul")
+            placaEsquerda.size = CGSize(width: 130, height: 93)
+            placaEsquerda.position = PosProporcional(pos: CGPoint(x: -80, y: 65))
+            placaEsquerda.zPosition = 10
+            
+            self.addChild(placaEsquerda)
+            
+            if navegação.Esteira.placasCores[1] == "vermelho" {
+                let placaDireita = SKSpriteNode(imageNamed: "vermelho")
+                placaDireita.size = CGSize(width: 130, height: 93)
+                placaDireita.position = PosProporcional(pos: CGPoint(x: 82, y: 65))
+                placaDireita.zPosition = 10
+                
+                self.addChild(placaDireita)
+            } else{
+                let placaDireita = SKSpriteNode(imageNamed: "verde")
+                placaDireita.size = CGSize(width: 130, height: 93)
+                placaDireita.position = PosProporcional(pos: CGPoint(x: 82, y: 65))
+                placaDireita.zPosition = 10
+                
+                self.addChild(placaDireita)
+            }
+        } else if navegação.Esteira.placasCores[0] == "verde" {
+            let placaEsquerda = SKSpriteNode(imageNamed: "verde")
+            placaEsquerda.size = CGSize(width: 130, height: 93)
+            placaEsquerda.position = PosProporcional(pos: CGPoint(x: -80, y: 65))
+            placaEsquerda.zPosition = 10
+            
+            let placaDireita = SKSpriteNode(imageNamed: "vermelho")
+            placaDireita.size = CGSize(width: 130, height: 93)
+            placaDireita.position = PosProporcional(pos: CGPoint(x: 82, y: 65))
+            placaDireita.zPosition = 10
+            
+            self.addChild(placaEsquerda)
+            self.addChild(placaDireita)
+        }
+        
+        let simbolo = SKSpriteNode(imageNamed: navegação.Esteira.simbolo)
+        simbolo.size = CGSize(width: 49, height: 160)
+        simbolo.position = PosProporcional(pos: CGPoint(x: 0, y: 30))
+        simbolo.zPosition = 10
+        
+        self.addChild(simbolo)
+        
+        //CUBOS
         let cubo1 = SKSpriteNode(imageNamed: "cuboancora")
         cubo1.size = CGSize(width: 39, height: 45)
         cubo1.position = PosProporcional(pos:CGPoint(x: -100, y: -161))
@@ -27,21 +75,27 @@ extension GameScene{
         cubo3.position = PosProporcional(pos:CGPoint(x: 18, y: -161))
         cubo3.zPosition = 10
         
-        let cubo4 = SKSpriteNode(imageNamed: "cuboaviao")
+        let cubo4 = SKSpriteNode(imageNamed: "cubolanterna")
         cubo4.size = CGSize(width: 39, height: 45)
         cubo4.position = PosProporcional(pos:CGPoint(x: 75, y: -161))
         cubo4.zPosition = 10
+        
+        let cubo5 = SKSpriteNode(imageNamed: "cubonuvem")
+        cubo5.size = CGSize(width: 39, height: 45)
+        cubo5.position = PosProporcional(pos:CGPoint(x: 135, y: -161))
+        cubo5.zPosition = 10
         
         self.addChild(cubo1)
         self.addChild(cubo2)
         self.addChild(cubo3)
         self.addChild(cubo4)
-
+        self.addChild(cubo5)
     }
     
     func TouchedEsteira(pos : CGPoint) {
         
     }
+    
 }
 
 
@@ -49,9 +103,11 @@ extension GameScene{
 struct EsteiraController {
     var ordemEsteira: [Int] = []
     var placasCores: [String] = []
+    var simbolo: String = ""
     
     init(){
         let placas = [1,2,3].randomElement()!
+        let simb = [1,2,3].randomElement()!
         
         switch placas{
         case 1:
@@ -63,6 +119,19 @@ struct EsteiraController {
         default:
             placasCores = ["azul", "vermelho"]
         }
+        
+        switch simb{
+            case 1:
+                simbolo = "symbolast"
+            case 2:
+                simbolo = "symbolestrela"
+            case 3:
+                simbolo = "symbolquadrado"
+            default:
+                simbolo = "symbolast"
+        }
     }
     
 }
+
+
